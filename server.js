a0565cd8fab64fa3144340d72fb6eb5573a7bd77
leandrocl2005 <leandrocl2005@yahoo.com.br>
2020-07-05 16:28:25 -0300

    pagination and groupy by SQL query

--- server.js
+++ server.js
@@ -40,23 +40,21 @@ nunjucks.configure("views", {
     nocache: true 
 })
 
+
 server.get("/", function(req, res) {
 
     db.all(`
-        SELECT * FROM imc
+        SELECT
+        COUNT(*) AS total
+        FROM
+            imc
+        GROUP BY
+            sexo
+        ORDER BY sexo ASC
     `, function(err, rows) {
 
-        if (err) {
-            return console.log(err)
-        }
-
-        const total_women = rows.filter(
-            (person) => person.sexo==0
-        ).length
-
-        const total_men = rows.filter(
-            (person) => person.sexo==1
-        ).length
+        const total_men = rows[1]["total"]
+        const total_women = rows[0]["total"]
 
         res.render("index.njk", {
             total_men,
@@ -69,6 +67,25 @@ server.get("/", function(req, res) {
 
 server.post("/", function(req, res) {
 
+    // get form data
+    let {weight, height, sexo} = req.body
+
+    // valid data
+    if (weight == "") {
+        return res.send("Preencher peso!")
+    }
+    
+    // format form data
+    weight = Number(weight.replace(',','.'))
+    //height = Number(height.replace(',','.'))
+    sexo = Number(sexo)
+
+    // save data
+    const values = [
+        height,
+        weight,
+        sexo
+    ]
     const query = `
         INSERT INTO imc (
             altura,
@@ -76,29 +93,22 @@ server.post("/", function(req, res) {
             sexo
         ) VALUES (?,?,?);
     `
-
-    // get form data
-    let {weight, height, sexo} = req.body
-    
-    // format form data
-    weight = Number(weight.replace(',','.'))
-    height = Number(height.replace(',','.'))
-    sexo = Number(sexo)
-
-    const values = [
-        height,
-        weight,
-        sexo
-    ]
-
     function afterInsert(err) { // don't use arrow funcs
     if (err) {
         return res.send("Erro no cadastro!")
     }
         return res.redirect("/")
     }   
-
     db.run(query, values, afterInsert)    
+})
+
+server.get("/table/:page", function(req, res) {
+    const {page} = req.params
+    db.all(`
+        select * from imc limit 4 offset ${page}
+    `, function(err, rows) {
+        res.render("table.njk", {rows})
+    })
 })
 
 server.listen(5000, function(){
